ad07bd40fc166f4dc0f95da26ba85184668598fd
tsahu <tsahu@egencia.com>
2023-02-16 18:57:16 +0530

    Updates

---
+++ precache-manifest.e89dd74662fde9fb290210af73c224dc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2640f8e65d717951bc311dd02b5bce",
+    "url": "/Valentines-Day/index.html"
+  },
+  {
+    "revision": "db24bc1eb06fdeb81a66",
+    "url": "/Valentines-Day/static/css/main.a5dce85a.chunk.css"
+  },
+  {
+    "revision": "9cbde23725ea0b206ceb",
+    "url": "/Valentines-Day/static/js/2.29ba7bba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Valentines-Day/static/js/2.29ba7bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db24bc1eb06fdeb81a66",
+    "url": "/Valentines-Day/static/js/main.fdb45ac9.chunk.js"
+  },
+  {
+    "revision": "17ae867271c0195b406a",
+    "url": "/Valentines-Day/static/js/runtime-main.f66799ab.js"
+  },
+  {
+    "revision": "10758bccc8c18750bf1ad61b271986da",
+    "url": "/Valentines-Day/static/media/Ruby.10758bcc.jpeg"
+  },
+  {
+    "revision": "d348e4401884dd1bb421bbe513fa54a7",
+    "url": "/Valentines-Day/static/media/pandinha.d348e440.gif"
+  },
+  {
+    "revision": "f1be026ff08aab1125b691c069d382f0",
+    "url": "/Valentines-Day/static/media/pandinha_coracao.f1be026f.gif"
+  },
+  {
+    "revision": "46feb314ef66fd9844e6c5343e228597",
+    "url": "/Valentines-Day/static/media/pandinha_fofa_cara_de_brava.46feb314.gif"
+  },
+  {
+    "revision": "8b0c4eecc314fa3131eae4e8ec70d06a",
+    "url": "/Valentines-Day/static/media/pandinha_martelo.8b0c4eec.png"
+  },
+  {
+    "revision": "865f1c977c2083c42cda7c223f72ea76",
+    "url": "/Valentines-Day/static/media/ursinha_chorando.865f1c97.gif"
+  },
+  {
+    "revision": "c4ff310ed4399ebe2c85ffc52ad5eeab",
+    "url": "/Valentines-Day/static/media/ursinha_mordidas.c4ff310e.gif"
+  },
+  {
+    "revision": "641a584c05345c733e8e5d94f906e53c",
+    "url": "/Valentines-Day/static/media/ursinho_S2_pandinha.641a584c.gif"
+  },
+  {
+    "revision": "7746f73129cc8fc7f46ada5e23f4d1b5",
+    "url": "/Valentines-Day/static/media/ursinho_catucando_pandinha.7746f731.gif"
+  },
+  {
+    "revision": "ee3bbcefdd762dc554b5db84df32bded",
+    "url": "/Valentines-Day/static/media/ursinho_chorando.ee3bbcef.gif"
+  },
+  {
+    "revision": "bde071c432bb0a09b34af0e0d5ffcd98",
+    "url": "/Valentines-Day/static/media/ursinho_estudando.bde071c4.gif"
+  },
+  {
+    "revision": "81e2399aba3f1ff6cd0c1df8eb967412",
+    "url": "/Valentines-Day/static/media/ursinho_pandinha_ok.81e2399a.gif"
+  },
+  {
+    "revision": "387281264ae4000e8a803a40169c1ac8",
+    "url": "/Valentines-Day/static/media/ursinho_pegando_no_sono.38728126.gif"
+  },
+  {
+    "revision": "04b613c4523e12dcb35d7f703a752faf",
+    "url": "/Valentines-Day/static/media/ursinhos_apaixonados.04b613c4.gif"
+  },
+  {
+    "revision": "e831c6c0da241c5146771d0233cfc219",
+    "url": "/Valentines-Day/static/media/ursinhos_bebida.e831c6c0.gif"
+  },
+  {
+    "revision": "b887f38a5ae81d5a3fe91244955386dc",
+    "url": "/Valentines-Day/static/media/ursinhos_caminhada.b887f38a.jpg"
+  },
+  {
+    "revision": "73252a13157ecdec88497a19c35256e5",
+    "url": "/Valentines-Day/static/media/ursinhos_dormindo.73252a13.gif"
+  },
+  {
+    "revision": "4aa4f4ad4538a66d10b1537ea06d994c",
+    "url": "/Valentines-Day/static/media/ursinhos_fotinha.4aa4f4ad.jpg"
+  }
+]);
